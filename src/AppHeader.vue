03c1cedd779706a9ac8fbb5f403325cf71576ff2
hakueiMac <king20010115@gmail.com>
2024-08-09 10:34:06 +0800

    first commit

---
+++ src/AppHeader.vue
@@ -0,0 +1,13 @@
+<template>
+    <header>
+        <h1>Vue 飲料店</h1>
+        <nav>
+            <ul>
+                <li>首頁</li>
+                <li>關於我們</li>
+                <li>產品</li>
+                <li>聯絡我們</li>
+            </ul>
+        </nav>
+    </header>
+</template>
